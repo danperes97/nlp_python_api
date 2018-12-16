67fe2d268063a55f2e9ca08512ea463d80eed796
DanHack <danilo.peres@hotmail.com>
2018-12-16 12:46:22 -0200

    First commit

---
+++ application_controller.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from json import dumps
+
+app = Flask(__name__)
+api = Api(app)
+
+import nltk
+import spacy
+
+def hashfy(ent):
+    return { 'text': ent.text, 'label': ent.label_ }
+
+class Nlp(Resource):
+    def get(self):
+        nlp = spacy.load("model_test.md")
+        text = nlp(request.args.get('query'))
+
+        return { 'ents': map(hashfy, text.ents) }
+
+api.add_resource(Nlp, '/test')
+
+if __name__ == '__main__':
+     app.run(port='3000')
